67028ee9411eebde9c0eb91053ce40fec361dcc2
Serabass <serabass565@gmail.com>
2015-10-04 15:00:54 +0600

    String::build in tsd

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,7 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="serialport/serialport.d.ts" />
+
+interface String {
+    build<T>(...params:any[]):T;
+}
