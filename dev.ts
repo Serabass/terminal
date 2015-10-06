5781fa3693b30bdf6744bf694acc2c3dace5a2c2
Serabass <serabass565@gmail.com>
2015-10-06 19:07:43 +0600

    dev.ts

--- dev.ts
+++ dev.ts
@@ -2,73 +2,86 @@
 
 import SerialPort from "serialport"
 import Command from "./command"
-import BCC from "./bcc"
+import Crypto from "./crypto"
+import * as events from "events";
 
-require('./utils');
+export default class Dev extends events.EventEmitter {
+    public port:string;
+    public serialPort:SerialPort;
+    public name:string;
+    public baudRate:number = 9600;
+    public bcc:boolean = false;
 
-export module Dev {
-    export class Dev {
-        public port:string;
-        public serialPort:SerialPort;
-        public name:string;
-        public baudRate:number = 9600;
-        public bcc:boolean = false;
+    constructor() {
+        super();
+    }
 
-        constructor() {
+    public isOpen():boolean {
+        return this.serialPort.isOpen();
+    }
 
-        }
+    public init({port, baudRate = this.baudRate}:{port:string, baudRate:number}):Promise<any> {
+        return new Promise((resolve:Function, reject:Function) => {
+            this.serialPort = new SerialPort({port, baudRate});
 
-        public isOpen():boolean {
-            return this.serialPort.isOpen();
-        }
-
-        public init(port:string, baudRate:number = this.baudRate):Promise<any> {
-            return new Promise((resolve:Function, reject:Function) => {
-                this.serialPort = new SerialPort({port, baudRate});
-
-                this.serialPort.open(function (err:any) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                    }
-                });
+            this.serialPort.open(function (err:any) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
             });
+        });
+    }
+
+    public getCommands():any {
+        return {};
+    }
+
+    public buildCommand(name:string, params:{}):Command {
+        var command:string = this.getCommands()[name];
+        var cmd:Command;
+
+        if ( command === void 0)
+            throw new Error(`Command ${name} not found`);
+
+        cmd = command.build<Command>(params);
+
+        if (this.bcc) {
+            cmd.bytes = this.appendBCC(cmd.bytes);
         }
 
-        public getCommands():any {
-            return {};
+        return cmd;
+    }
+
+    public appendBCC(data:Buffer):Buffer {
+        let bcc:number = Crypto.getBCC(data);
+        let arr:Buffer[] = [data, new Buffer([bcc])];
+        return Buffer.concat(arr);
+    }
+
+    public stop(fn:Function) {
+        this.serialPort.close(fn);
+    }
+
+    public send(name:any, params:any = {}):Promise<any> {
+        var cmd:Command;
+
+        if (name instanceof Command) {
+            cmd = name;
+        } else {
+            cmd = this.buildCommand(name, params);
         }
 
-        public stop() {}
+        return new Promise<any>((reject:Function, resolve:Function) => {
+            this.serialPort.write(cmd.bytes, (err:any, results:any):void => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
 
-        public send(name:string, ...params:any[]):Promise<any> {
-
-            var command:string = this.getCommands()[name];
-
-            if ( command === void 0)
-                throw new Error(`Command ${name} not found`);
-
-            var cmd:Command = command.build<Command>(...params);
-
-            if (this.bcc) {
-                let bcc:number = BCC.build(cmd.bytes);
-                let arr:Buffer[] = [cmd.bytes, new Buffer([bcc])];
-                cmd.bytes = Buffer.concat(arr);
-            }
-
-            /// ...logic
-
-            return new Promise<any>((reject:Function, resolve:Function) => {
-                this.serialPort.write(cmd.bytes, (err:any, results:any):void => {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-
-                    resolve(results);
-                });
+                resolve(results);
             });
-        }
+        });
     }
 }
