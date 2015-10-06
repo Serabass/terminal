13ef1cf198c31d99fc3b0e67c4ec1c159e1e7401
Serabass <serabass565@gmail.com>
2015-10-07 03:56:11 +0600

    delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-require('./utils');
-var lcdm2000_1 = require('./devices/lcdm2000');
-var settings2_1 = require('./settings2');
-var l = new lcdm2000_1.default();
-console.log(settings2_1.default.get());
-//# sourceMappingURL=index.js.map
