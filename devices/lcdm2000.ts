f8c8bad91a29da843b0957f8a06898866c36c333
Serabass <serabass565@gmail.com>
2015-10-06 19:07:05 +0600

    added lcdm2000

---
+++ devices/lcdm2000.ts
@@ -0,0 +1,63 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import Dev from '../dev';
+import Command from "../command";
+
+export default class LCDM2000 extends Dev {
+
+    private _levels:any = {
+        u: 0x45,
+        l: 0x55,
+        both : 0x56
+    };
+
+    private _id:number = 0x01;
+
+    public getCommands():{} {
+        return {
+            dispense       : [
+                '.EOT'          ,
+                '@id'           ,
+                '.STX'          ,
+                '@level'        ,
+                '@decimals'     ,
+                '@bill'         ,
+                '.ETX'
+            ].join(' '),
+            dispenseBoth   : [
+                '.EOT'          ,
+                '@id'           ,
+                '.STX'          ,
+                '@level'        ,
+                '@uDecimals'    ,
+                '@uBill'        ,
+                '@lDecimals'    ,
+                '@lBill'        ,
+                '.ETX'
+            ].join(' '),
+        };
+    }
+
+    public buildDispense(level:string, count:number):Command { //:Promise<any> {
+
+        var bill:number,
+            decimals:number,
+            lvl:number = this._levels[level]
+        ;
+
+        decimals = Math.floor(count / 10) + 0x30;
+        bill = (count % 10) + 0x30;
+
+        return this.buildCommand('dispense', {
+            level: lvl,
+            decimals,
+            bill,
+            id: this._id
+        });
+    }
+
+    public runDispense(level:string, count:number):Promise<any> {
+        var cmd:Command = this.buildDispense(level, count);
+        return this.send(cmd);
+    }
+}
