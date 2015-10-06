e2163ad83c5e293732ed3bae53368ac5d8906565
Serabass <serabass565@gmail.com>
2015-10-07 04:51:42 +0600

    sp

--- typings/serialport/serialport.d.ts
+++ typings/serialport/serialport.d.ts
@@ -1,9 +1,7 @@
-declare module "serialport" {
-    export default class SerialPort {
-        constructor(options:any);
-        isOpen():boolean;
-        open(fn:Function):void;
-        write(data:(String | Buffer), callback:(err:any, results:any) => void):void;
-        close(fn:Function):void;
-    }
+declare class SerialPort {
+    constructor(options:any);
+    isOpen():boolean;
+    open(fn:Function):void;
+    write(data:(String | Buffer), callback:(err:any, results:any) => void):void;
+    close(fn:Function):void;
 }
