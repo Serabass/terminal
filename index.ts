c44cf0db95f72072c4cf5565e8d876b9bf663591
Serabass <serabass565@gmail.com>
2015-10-06 19:04:07 +0600

    index

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 require('./utils');
 
 import CommandBuilder from './command-builder'
+import LCDM2000 from './devices/lcdm2000'
+import Settings from './settings'
 
-console.log('.NUL @1'.build(0xFF));
+var l = new LCDM2000();
+
+console.log(Settings.get());
