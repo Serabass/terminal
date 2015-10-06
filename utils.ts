26017db8835c4e3ee5d6c2ac8e20064969579c3f
Serabass <serabass565@gmail.com>
2015-10-06 19:05:08 +0600

    updated utils.ts

--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 import Command from "./command"
 import CommandBuilder from './command-builder'
 
-String.prototype.build = function (...params:any[]):Command {
-    return CommandBuilder.build(this, ...params);
+String.prototype.build = function (params:any[]):Command {
+    return CommandBuilder.build(this, params);
 };
