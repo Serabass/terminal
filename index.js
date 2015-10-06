63d7203a50a87537e7516ad1d31fe5359f77b949
Serabass <serabass565@gmail.com>
2015-10-06 19:25:16 +0600

    fuck

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('./utils');
 var lcdm2000_1 = require('./devices/lcdm2000');
-var settings_1 = require('./settings');
+var settings2_1 = require('./settings2');
 var l = new lcdm2000_1.default();
-console.log(settings_1.default.get());
+console.log(settings2_1.default.get());
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ require('./utils');
 
 import CommandBuilder from './command-builder'
 import LCDM2000 from './devices/lcdm2000'
-import Settings from './settings'
+import Settings from './settings2'
 
 var l = new LCDM2000();
 
